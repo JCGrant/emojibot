0e92a06cda22555b96e60733ae4754f525c3c27d
JCGrant <jamescolin.grant@gmail.com>
2018-12-11 19:23:01 +0000

    emojify should return the unicode form of the emoji

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ map_id_to_emoji = []
 with open('{}/emoji_unicode.csv'.format(ROOT_PATH), 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        map_id_to_emoji.append(row[1])
+        map_id_to_emoji.append(row[0])
 
 def get_emoji(i):
     if i < 0 or i >= len(map_id_to_emoji):
@@ -13,4 +13,5 @@ def get_emoji(i):
     return map_id_to_emoji[i]
 
 def emojify(t_score):
-    return '&#x{}'.format(get_emoji(t_score['ids'][0]))
+    i = t_score['ids'][0]
+    return get_emoji(i).decode('unicode-escape')
